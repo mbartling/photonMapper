fd5c84797604f0754abf856c7d83f3f9a8d4d1f4
Michael Bartling <michael.bartling15+dev@gmail.com>
2016-04-21 16:21:15 -0500

    Pretty sure I finished the spatial hash

---
+++ src/scene/spatialHash.h
@@ -0,0 +1,21 @@
+#include <tuple>
+#include <unordered_map>
+#include "photon.h"
+#include "../vecmath/vec.h"
+
+template <typename T>
+class mHash{
+  float radius;
+
+  public:
+    mHash(float r): radius(r){}
+
+    // Update this part to support whatever type we use for Point
+    std::tuple<int, int, int> operator() ( T p ){
+      return std::make_tuple( int(p.x/radius), int(p.y/radius), int(p.z/radius))
+    }
+
+};
+
+
+// spatialHash = unordered_map<Point, Photon, mHash>(); //Create the Spatial Hash
